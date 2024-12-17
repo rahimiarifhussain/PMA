643cd3aeddb0fe38d2ed0e8a81c1512269f918df
rahimiarifhussain <arifhussain.rahimi84@gmail.com>
2024-12-17 13:57:30 +0430

    add basic route

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,22 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+
+Route::redirect('/', '/dashboard');
+Route::middleware('auth', 'verified')->group(function () {
+    Route::get('/dashboard', fn()=>Inertia::render('Dashboard'))->name('dashboard');
+    Route::resource('project', ProjectController::class);
+    Route::resource('task', TaskController::class);
+    Route::resource('user', UserController::class);
 });
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
